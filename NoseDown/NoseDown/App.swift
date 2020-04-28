a5a92b9dfd1f96adc55d7bd97a109b6a3d47156e
RaulSul <raul.sulaimanov@gmail.com>
2020-04-28 12:13:15 +0200

    restyled buttons. Added squats button. refactored button creation function

--- NoseDown/NoseDown/App.swift
+++ NoseDown/NoseDown/App.swift
@@ -1,5 +1,5 @@
 //
-//  AppDelegate.swift
+//  App.swift
 //  NoseDown
 //
 //  Created by Raul (Private) on 13.04.20.
@@ -18,9 +18,11 @@ import CoreData
     var mainVC : UIViewController?
     var settings: UIViewController?
     var pushUpsVC: UIViewController?
+    var squatsVC: UIViewController?
     
     // Buttons:
     var pushUpsButton: UIButton?
+    var squatsButton: UIButton?
     var settingsButton: UIButton?
     
     //MARK: - View
@@ -32,25 +34,34 @@ import CoreData
         pushViewController(mainVC, animated: false)
         self.mainVC = mainVC
         
-        let pushUpsButton = button("Push-Ups", #selector(openPushUpsVC))
+        let pushUpsButton = button("pushups", #selector(openPushUpsVC))
         mainVC.view.addSubview(pushUpsButton)
         self.pushUpsButton = pushUpsButton
         
-        let settingsButton = button("Settings", #selector(openSettings))
+        let squatsButton = button("squats", #selector(opensquatsVC))
+        mainVC.view.addSubview(squatsButton)
+        self.squatsButton = squatsButton
+        
+        let settingsButton = button("settings", #selector(openSettings))
         mainVC.view.addSubview(settingsButton)
         self.settingsButton = settingsButton
         
         
         //MARK: - Layout
-        pushUpsButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
-        pushUpsButton.heightAnchor.constraint(equalToConstant: 20).isActive = true
-        pushUpsButton.leftAnchor.constraint(equalTo: mainVC.view.safeAreaLayoutGuide.leftAnchor).isActive = true
+        pushUpsButton.widthAnchor.constraint(equalToConstant: 30).isActive = true
+        pushUpsButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        pushUpsButton.leftAnchor.constraint(equalTo: mainVC.view.safeAreaLayoutGuide.leftAnchor, constant: 30).isActive = true
         pushUpsButton.bottomAnchor.constraint(equalTo: mainVC.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
         
-        settingsButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
-        settingsButton.heightAnchor.constraint(equalToConstant: 20).isActive = true
-        settingsButton.rightAnchor.constraint(equalTo: mainVC.view.safeAreaLayoutGuide.rightAnchor).isActive = true
-        settingsButton.bottomAnchor.constraint(equalTo: mainVC.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        squatsButton.widthAnchor.constraint(equalToConstant: 30).isActive = true
+        squatsButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        squatsButton.rightAnchor.constraint(equalTo: mainVC.view.safeAreaLayoutGuide.rightAnchor, constant: -30).isActive = true
+        squatsButton.bottomAnchor.constraint(equalTo: mainVC.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        
+        settingsButton.widthAnchor.constraint(equalToConstant: 30).isActive = true
+        settingsButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        settingsButton.rightAnchor.constraint(equalTo: mainVC.view.safeAreaLayoutGuide.rightAnchor, constant: -30).isActive = true
+        settingsButton.topAnchor.constraint(equalTo: mainVC.view.safeAreaLayoutGuide.topAnchor, constant: 30).isActive = true
     }
 
     //MARK: - Application Functions
@@ -89,12 +100,14 @@ import CoreData
     
     //MARK: - Miscellanious
     
-    func button(_ title: String, _ selector: Selector) -> UIButton {
+    func button(_ imageName: String, _ selector: Selector) -> UIButton {
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
         button.clipsToBounds = true
         button.addTarget(self, action: selector, for: .touchUpInside)
-        button.setTitle(title, for: [])
+//        button.setTitle(title, for: [])
+        button.setImage(UIImage(named: imageName), for: [])
+        button.imageView?.contentMode = .scaleAspectFit
         
         return button
     }
@@ -104,6 +117,14 @@ import CoreData
         
         if viewControllers.count < 2 {
             pushViewController(pushUpsVC, animated: true)
+        }
+    }
+    
+    @objc func opensquatsVC() {
+        guard let squatsVC = squatsVC else { return }
+        
+        if viewControllers.count < 2 {
+            pushViewController(squatsVC, animated: true)
         }
     }
     
