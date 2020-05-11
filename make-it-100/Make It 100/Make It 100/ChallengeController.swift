d8e2d4fb3b9a90626c4d930cc6599650d07faf76
RaulSul <raul.sulaimanov@gmail.com>
2020-05-11 17:31:16 +0200

    added repeat counter to the challenge controller class

--- make-it-100/Make It 100/Make It 100/ChallengeController.swift
+++ make-it-100/Make It 100/Make It 100/ChallengeController.swift
@@ -15,6 +15,8 @@ class ChallengeController: UIViewController {
     
     // Record Keeping:
     var currentRepeat: Int = 0
+    var repeatLabel: UILabel?
+    
     var currentSet: Int = 0
     var currentWeek: Int = 0
     var pauseTimer: Float = 0
@@ -32,15 +34,31 @@ class ChallengeController: UIViewController {
         view.addSubview(repeatButton)
         self.repeatButton = repeatButton
         
+        let repeatLabel: UILabel = UILabel()
+        repeatLabel.translatesAutoresizingMaskIntoConstraints = false
+        repeatLabel.clipsToBounds = true
+        repeatLabel.text = String(currentRepeat)
+        repeatLabel.textAlignment = .center
+        repeatLabel.backgroundColor = .blue
+        view.addSubview(repeatLabel)
+        self.repeatLabel = repeatLabel
+        
         //MARK: - Layout
         repeatButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         repeatButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         repeatButton.widthAnchor.constraint(equalToConstant: 150).isActive = true
         repeatButton.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        
+        repeatLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        repeatLabel.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        repeatLabel.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        repeatLabel.bottomAnchor.constraint(equalTo: repeatButton.topAnchor, constant: 15).isActive = true
     }
     
     @objc func repeatButtonPress() {
-        print(">> RepeatButton pressed")
+        currentRepeat += 1
+        repeatLabel?.text = String(currentRepeat)
+        print("Current repeat is: \(currentRepeat)")
     }
     
     
